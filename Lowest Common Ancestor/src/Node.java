a2eae15dfd4d162fbd7672bb894c1a5dc7064ab7
Sharleen Bang <bangs@tcd.ie>
2018-09-29 20:30:11 +0100

    Node Class - Added Constructor, Getters & Setter Functions

--- Lowest Common Ancestor/src/Node.java
+++ Lowest Common Ancestor/src/Node.java
@@ -1,5 +1,35 @@
 
 public class Node {
-	Node(){
+	private int value;
+	private int numSubNodes;
+	private Node nextNode;
+	private Node prevNode;
+	Node(int value, int n){
+		setValue(value);
+		setNumSubNodes(n);
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	public int getNumSubNodes() {
+		return numSubNodes;
+	}
+	public void setNumSubNodes(int numSubNodes) {
+		this.numSubNodes = numSubNodes;
+	}
+	public Node getNextNode() {
+		return nextNode;
+	}
+	public void setNextNode(Node nextNode) {
+		this.nextNode = nextNode;
+	}
+	public Node getPrevNode() {
+		return prevNode;
+	}
+	public void setPrevNode(Node prevNode) {
+		this.prevNode = prevNode;
 	}
 }
