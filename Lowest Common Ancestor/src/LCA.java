bf8d99949a80f41ede2887f665c0657cacc5fd7e
Sharleen Bang <bangs@tcd.ie>
2018-09-30 15:31:45 +0100

    findPath() testing in progress

--- Lowest Common Ancestor/src/LCA.java
+++ Lowest Common Ancestor/src/LCA.java
@@ -3,39 +3,35 @@ import java.util.ArrayList;
 public class LCA {
 	public ArrayList<Integer> path1 = new ArrayList<Integer>();
 	public ArrayList<Integer> path2 = new ArrayList<Integer>();
-	
-	public static boolean pathExists(Node current, Node dst){
-		if(current == null || dst == null){
+
+	public static boolean pathExists(Node current, Node dst) {
+		if (current == null || dst == null) {
 			return false;
 		}
-		if(current.getValue() == dst.getValue()){
+		if (current.getValue() == dst.getValue()) {
 			return true;
 		}
-		if(current.getLeftNode() != null && pathExists(current.getLeftNode(), dst)){
+		if (current.getLeftNode() != null && pathExists(current.getLeftNode(), dst)) {
 			return true;
 		}
-		if(current.getRightNode() != null && pathExists(current.getRightNode(), dst)){
+		if (current.getRightNode() != null && pathExists(current.getRightNode(), dst)) {
 			return true;
 		}
 		return false;
 	}
-	
-	public static ArrayList<Integer> findPath(Node current, Node dst, ArrayList<Integer> pathStore){
-		if(pathExists(current, dst)){
-			if(current == null || dst == null){
-				return pathStore;
-			}
-			if(current.getValue() == dst.getValue()){
-				return pathStore;
-			}
-			if(current.getLeftNode() != null){
-				pathStore.add(current.getLeftNode().getValue());
+
+	public static ArrayList<Integer> findPath(Node current, Node dst, ArrayList<Integer> pathStore) {
+		if (pathExists(current, dst)) {
+			pathStore.add(current.getValue());
+			if (current.getLeftNode() != null) {
 				return findPath(current.getLeftNode(), dst, pathStore);
 			}
-			if(current.getRightNode() != null ){
-				pathStore.add(current.getRightNode().getValue());
+			if (current.getRightNode() != null) {
 				return findPath(current.getRightNode(), dst, pathStore);
-			}		
+			}
+			if (current.getLeftNode() == null && current.getRightNode() == null || current.getValue() == dst.getValue()) {
+				return pathStore;
+			}
 		}
 		return pathStore;
 	}
--- Lowest Common Ancestor/src/LCATest.java
+++ Lowest Common Ancestor/src/LCATest.java
@@ -1,26 +1,84 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class LCATest {
 
 	@Test
 	public void testNodeCreation() {
-		/*Check Value*/
-		Node node3 = new Node(3); 
+		/* Check Value */
+		Node node3 = new Node(3);
 		assertEquals(3, node3.getValue());
-		
+
 		Node node2 = new Node(2);
 		assertEquals(2, node2.getValue());
-		
-		/*Create a Tree*/
+
+		/* Create a Tree */
 		Node root = new Node(1);
-		
-		/* Left Side of Tree*/
+
+		/* Left Side of Tree */
 		root.setLeftNode(node2);
 		node2.setRightNode(new Node(4));
-		
-		/*Right Side of Tree*/
+
+		/* Right Side of Tree */
+		root.setRightNode(node3);
+		node3.setRightNode(new Node(6));
+		Node node5 = new Node(5);
+		node3.setLeftNode(node5);
+		node5.setLeftNode(new Node(7));
+		node5.setRightNode(new Node(8));
+
+		/* Test the tree */
+		assertEquals(8, node5.getRightNode().getValue());
+		assertEquals(4, node2.getRightNode().getValue());
+		assertEquals(2, root.getLeftNode().getValue());
+		assertEquals(3, root.getRightNode().getValue());
+	}
+
+	@Test
+	public void testPathExists() {
+		/* Create a Tree */
+		Node root = new Node(1);
+		Node node2 = new Node(2);
+		Node node3 = new Node(3);
+
+		/* Left Side of Tree */
+		root.setLeftNode(node2);
+		node2.setRightNode(new Node(4));
+
+		/* Right Side of Tree */
+		root.setRightNode(node3);
+		node3.setRightNode(new Node(6));
+		Node node5 = new Node(5);
+		node3.setLeftNode(node5);
+		node5.setLeftNode(new Node(7));
+		node5.setRightNode(new Node(8));
+
+		assertTrue(LCA.pathExists(root, node2));
+		assertTrue(LCA.pathExists(root, node2.getRightNode()));
+
+		assertTrue(LCA.pathExists(root, root.getRightNode()));
+
+		assertTrue(LCA.pathExists(root, node5.getRightNode()));
+		assertTrue(LCA.pathExists(root, node5.getLeftNode()));
+		assertTrue(LCA.pathExists(root, node2.getRightNode()));
+		assertFalse(LCA.pathExists(root, node2.getLeftNode()));
+	}
+
+	@Test
+	public void testFindPath() {
+		/* Create a Tree */
+		Node root = new Node(1);
+		Node node2 = new Node(2);
+		Node node3 = new Node(3);
+
+		/* Left Side of Tree */
+		root.setLeftNode(node2);
+		node2.setRightNode(new Node(4));
+
+		/* Right Side of Tree */
 		root.setRightNode(node3);
 		node3.setRightNode(new Node(6));
 		Node node5 = new Node(5);
@@ -28,42 +86,19 @@ public class LCATest {
 		node5.setLeftNode(new Node(7));
 		node5.setRightNode(new Node(8));
 		
-		/*Test the tree*/
-		assertEquals(8, node5.getRightNode().getValue());
-		assertEquals(4, node2.getRightNode().getValue());
-		assertEquals(2, root.getLeftNode().getValue());
-		assertEquals(3, root.getRightNode().getValue());
+		ArrayList<Integer> result1 = new ArrayList<Integer>();
+		ArrayList<Integer> expectedVal1 = new ArrayList<Integer>();
+		expectedVal1.add(1);
+		expectedVal1.add(2);
+		expectedVal1.add(4);
+		assertEquals(expectedVal1, LCA.findPath(root, node2.getRightNode(), result1));
+		
+		ArrayList<Integer> result2 = new ArrayList<Integer>();
+		ArrayList<Integer> expectedVal2 = new ArrayList<Integer>();
+		expectedVal2.add(1);
+		expectedVal2.add(3);
+		expectedVal2.add(6);
+		assertEquals(expectedVal2, LCA.findPath(root, node3.getRightNode(), result2));
 	}
-	
-	@Test 
-	public void testPathExists(){
-		/*Create a Tree*/
-		Node root = new Node(1);
-		Node node2 = new Node(2);
-		Node node3 = new Node(3);
-		
-		/* Left Side of Tree*/
-		root.setLeftNode(node2);
-		node2.setRightNode(new Node(4));
-		
-		/*Right Side of Tree*/
-		root.setRightNode(node3);
-		node3.setRightNode(new Node(6));		
-		Node node5 = new Node(5);
-		node3.setLeftNode(node5);
-		node5.setLeftNode(new Node(7));
-		node5.setRightNode(new Node(8));
-		
-		assertTrue(LCA.pathExists(root, node2));
-		assertTrue(LCA.pathExists(root, node2.getRightNode()));
-		
-		assertTrue(LCA.pathExists(root, root.getRightNode()));
-		
-		assertTrue(LCA.pathExists(root, node5.getRightNode()));
-		assertTrue(LCA.pathExists(root, node5.getLeftNode()));
-		assertTrue(LCA.pathExists(root, node2.getRightNode()));
-		assertFalse(LCA.pathExists(root, node2.getLeftNode()));		
-		
-	}
-	
+
 }
